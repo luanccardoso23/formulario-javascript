626c84987931db469483b0abb44509b2e39e35b9
Luan Cardoso <luanccardoso23@gmail.com>
2022-05-09 20:36:32 -0300

    Continuando projeto javascript

--- js/script.js
+++ js/script.js
@@ -28,3 +28,25 @@ let leitura = document.querySelector("input[readonly]")
 endereco.addEventListener("keyup",function(){
     leitura.value = endereco.value
 })
+
+
+/* VALIDAR EMAIL*/
+let email = document.querySelector("#email")
+let msgEmail = document.querySelector("#validaEmail")
+
+// Trabalhando com Arrow Function
+email.addEventListener('keyup', ()=>{
+    let minusculo = email.value.toLowerCase()// Convertendo o Texto em minusculo
+
+    if(minusculo.indexOf('@') == -1 || minusculo.indexOf(".com")==-1 ){
+        //console.log("Email Inválido")
+        msgEmail.textContent = "Este email é inválido"
+        msgEmail.classList.remove("text-success")
+        msgEmail.classList.add("text-danger")
+    }
+    else{
+        //console.log("Email Válido")
+        msgEmail.classList.remove("text-danger")
+        msgEmail.classList.add("text-success")
+    }
+})
