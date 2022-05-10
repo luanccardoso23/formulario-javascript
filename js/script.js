eb53a61495a8119d73586d6032e95fb3c462a50e
Luan Cardoso <luanccardoso23@gmail.com>
2022-05-10 20:55:57 -0300

    Finalizado validação de senha

--- js/script.js
+++ js/script.js
@@ -50,3 +50,27 @@ email.addEventListener('keyup', ()=>{
         msgEmail.classList.add("text-success")
     }
 })
+
+/* HABILITAR VIZUALIZAÇÃO DA SENHA */
+let senha = document.querySelector("#senha")
+let iconeOlho = document.querySelector("#senha + span")// estou pegando a tag span exatamente após a tag que possui um id="senha."
+
+iconeOlho.addEventListener("click",()=>{
+    //console.log(iconeOlho)
+    if(iconeOlho.textContent == "visibility"){
+        iconeOlho.textContent = "visibility_off"
+        senha.setAttribute("type","password")
+    }
+    else{
+        iconeOlho.textContent = "visibility"
+        senha.setAttribute("type","text")
+    }
+})
+
+/* PEGANDO INFORMAÇÕES DA RÁDIO */
+let escolaridade = document.querySelectorAll("input[name-escolaridade]")
+let btnEscolaridade = document.querySelector("#btnEscolaridade")
+
+btnEscolaridade.addEventListener("click",()=>{
+    console.log(escolaridade)
+})
